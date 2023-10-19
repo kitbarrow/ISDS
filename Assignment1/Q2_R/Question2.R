8033834fc282a685fc7f61a7a8d5a9239d2d6bd5
Qian Zehao <qianzehao123@gmail.com>
2023-10-19 15:11:33 +0100

    R script for question 2

---
+++ Assignment1/Q2_R/Question2.R
@@ -0,0 +1,26 @@
+# clear the console area
+cat("\014")
+# clear the environment var area
+rm(list = ls())
+# Load the Tooth Growth data set
+data(ToothGrowth)
+
+# Find the modal tooth length
+modal_tooth_length1 = as.numeric(names(sort(
+  table(ToothGrowth$len), decreasing = TRUE)[1]))
+
+
+# Calculate the mean tooth length for guinea pigs 
+# given vitamins via orange juice
+mean_tooth_length_orange = mean(
+  ToothGrowth$len[ToothGrowth$supp == "OJ"])
+mean_tooth_length_orange
+
+# Create a side-by-side box-and-whisker plot
+# for tooth length by supplement type
+boxplot(len ~ supp, data = ToothGrowth, 
+        col = c("lightblue", "lightgreen"),
+        xlab = "Supplement Type", ylab = "Tooth Length",
+        main = "Tooth Length by Supplement Type")
+legend("topright", legend = c("Ascorbic Acid", "Orange Juice"),
+       fill = c("lightblue", "lightgreen"))
