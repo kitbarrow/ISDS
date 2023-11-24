fc2f92fa7d92cad7a2f7573fc79210c65b813806
Qian Zehao <qianzehao123@gmail.com>
2023-11-24 11:18:26 +0000

    missing value handle with MICE

---
+++ group-mini-report/MissingValueHandling.R
@@ -0,0 +1,19 @@
+library(mice)
+# -----------------------------------------------------------------------------
+# clear the console area
+cat("\014")
+# clear the environment var area
+rm(list = ls())
+# clear all plots
+graphics.off()
+# -----------------------------------------------------------------------------
+# current_directory
+current_directory <- getwd()
+file_path <- file.path(current_directory, "Happyedt.csv")
+Happy <- read.csv(file_path)
+# -----------------------------------------------------------------------------
+mean(Happy$HLE)
+
+imputed_data <- mice(Happy)
+completed_data <- complete(imputed_data)
+head(completed_data)
